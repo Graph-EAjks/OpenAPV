c140526d2861c8f1387ab2fbc0ebf6a54d08c387
subhrajitm20 <2003subhrajit@gmail.com>
2025-06-04 17:41:26 +0530

    single pass Fwd Tx AVX-code update
    Signed-off-by: subhrajitm20 <2003subhrajit@gmail.com>

--- src/avx/oapv_tq_avx.h
+++ src/avx/oapv_tq_avx.h
@@ -129,6 +129,7 @@
 
 
 #if X86_SSE
+extern const oapv_fn_tx_part_t oapv_tbl_fn_txb_part_avx[2];
 extern const oapv_fn_tx_t oapv_tbl_fn_txb_avx[2];
 extern const oapv_fn_quant_t oapv_tbl_fn_quant_avx[2];
 extern const oapv_fn_itx_part_t oapv_tbl_fn_itx_part_avx[2];
